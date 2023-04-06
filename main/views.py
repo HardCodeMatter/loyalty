3e899a11b7e4c7650ffb66a26c76bfe711fcb9cf
hardline-dev <dklymovets1@gmail.com>
2023-04-06 21:36:25 +0300

    Created Card list layout and added button for detail views.

--- main/views.py
+++ main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .services import CardService
 
 
 def card_list_view(request):
+    service = CardService()
+    cards = service.get_objects()
 
-    context = {}
+    context = {
+        'cards': cards
+    }
 
     return render(request, 'main/card_list.html', context)
