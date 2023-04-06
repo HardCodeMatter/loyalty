6904d524a63424749cb0f2f3ca3ad5980b540010
hardline-dev <dklymovets1@gmail.com>
2023-04-06 11:08:35 +0300

    Created Card model and configured admin panel.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Card
 
-# Register your models here.
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('number', 'owner', 'date_released', 'date_expired', 'balance',)
+    list_filter = ('number', 'owner', 'date_released', 'date_expired', 'balance',)
+
+    fieldsets = [
+        (None, {
+            'fields': ('number', 'owner', 'balance',),
+        }),
+        ('Date and time', {
+            'fields': ('date_released', 'date_expired',),
+        }),
+        ('Card status', {
+            'fields': ('is_activated', 'is_expired',),
+        }),
+    ]
+
+    search_fields = ('number', 'owner', 'date_released', 'date_expired', 'balance',)
+    ordering = ('-id',)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Card(models.Model):
+    number = models.IntegerField(('number'), unique=True, blank=True)
+    owner = models.CharField(('owner'), max_length=100)
+    date_released = models.DateTimeField(('date released'), default=timezone.now)
+    date_expired = models.DateTimeField(('date expired'))
+    balance = models.FloatField(('balance'))
+    
+    is_activated = models.BooleanField(('activated'), default=False)
+    is_expired = models.BooleanField(('expired'), default=False)
+
+    def __str__(self):
+        return f'Card {self.pk}'
