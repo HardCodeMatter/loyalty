686efece3040fde00adc70193a5f8d1cc0ba5c8b
hardline-dev <dklymovets1@gmail.com>
2023-04-07 22:54:53 +0300

    Created CardHistory model and configured admin panel.

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Card
+from .models import Card, CardHistory
+
+
+class CardHistoryInline(admin.StackedInline):
+    model = CardHistory
+    extra = 0
 
 
 @admin.register(Card)
@@ -20,4 +25,26 @@ class CardAdmin(admin.ModelAdmin):
     ]
 
     search_fields = ('number', 'owner', 'date_released', 'date_expired', 'balance',)
+    ordering = ('-id',)
+    inlines = (CardHistoryInline,)
+
+
+@admin.register(CardHistory)
+class CardHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'card', 'bonus', 'balance', 'date_used',)
+    list_filter = ('id', 'card', 'bonus', 'balance', 'date_used',)
+
+    fieldsets = [
+        (None, {
+            'fields': ('card',),
+        }),
+        ('Date and time', {
+            'fields': ('date_used',),
+        }),
+        ('Card balance', {
+            'fields': ('bonus', 'balance',),
+        }),
+    ]
+
+    search_fields = ('id', 'card', 'bonus', 'balance', 'date_used',)
     ordering = ('-id',)
--- main/models.py
+++ main/models.py
@@ -12,5 +12,24 @@ class Card(models.Model):
     is_activated = models.BooleanField(('activated'), default=False)
     is_expired = models.BooleanField(('expired'), default=False)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'Card {self.pk}'
+    
+    class Meta:
+        verbose_name = 'card'
+        verbose_name_plural = 'cards'
+
+
+class CardHistory(models.Model):
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    label = models.CharField(('label'), max_length=100)
+    bonus = models.FloatField(('bonus'))
+    balance = models.FloatField(('balance'))
+    date_used = models.DateTimeField(('date used'), default=timezone.now)
+
+    def __str__(self) -> str:
+        return f'Card {self.card}'
+
+    class Meta:
+        verbose_name = 'card history'
+        verbose_name_plural = 'card histories'
