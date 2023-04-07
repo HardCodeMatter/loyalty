16ebec1de69438952d85095f0a1fd7498870cd17
hardline-dev <dklymovets1@gmail.com>
2023-04-07 22:37:29 +0300

    Added annotation for views and services.

--- main/services.py
+++ main/services.py
@@ -1,20 +1,21 @@
 from .models import Card
+from typing import List, Dict
 
 
 class CardService:
-    def get_objects(self):
+    def get_objects(self) -> List[Card]:
         """Returns a queryset of all Card records in the database"""
         return Card.objects.all()
     
-    def get_object_by_id(self, id):
+    def get_object_by_id(self, id: int) -> Card:
         """Returns a single Card record by id"""
         return Card.objects.get(id=id)
     
-    def create_object(self, data):
+    def create_object(self, data: Dict) -> Card:
         """Creates a new Card record in the database"""
         return Card.objects.create(**data)
     
-    def update_object(self, id, data):
+    def update_object(self, id: int, data: Dict) -> Card:
         """Updates an existing Card record in the database"""
         object = Card.objects.get(id=id)
 
@@ -25,7 +26,7 @@ class CardService:
 
         return object
 
-    def delete_object(self, id):
+    def delete_object(self, id: int) -> None:
         """Deletes an existing Card record in the database"""
         object = Card.objects.get(id=id)
         object.delete()
--- main/views.py
+++ main/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Card
 from .services import CardService
+from typing import List
 
 
-def card_list_view(request):
+def card_list_view(request: HttpResponse) -> HttpResponse:
     service = CardService()
-    cards = service.get_objects()
+    cards: List[Card] = service.get_objects()
 
     context = {
         'cards': cards
@@ -12,9 +14,9 @@ def card_list_view(request):
 
     return render(request, 'main/card_list.html', context)
 
-def card_detail_view(request, id):
+def card_detail_view(request: HttpResponse, id: int) -> HttpResponse:
     service = CardService()
-    card = service.get_object_by_id(id)
+    card: Card = service.get_object_by_id(id)
 
     context = {
         'card': card
